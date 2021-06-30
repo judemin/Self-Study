f6c05f7e86597ab4bc84b44753a6358ee61d41f1
민상연 <judemin2087@naver.com>
2021-06-30 15:05:18 +0900

    Seminar 12 - Iterator
    인프런 타입스크립트 코리아 기초

---
+++ typescript-basic/week3-s12.ts
@@ -0,0 +1,24 @@
+/*
+* Seminar 11 Iterator
+*/
+/// Custon Iterable ///
+class CustomIterable implements Iterable<string>{
+    private _array : Array<string> = ["first","second"];
+
+    [Symbol.iterator]() {
+        var nextIndex = 0;
+
+        return {
+            next : () =>{
+                return {
+                    value : this._array[nextIndex++],
+                    done: nextIndex < this._array.length
+                }
+            }
+        }
+    }
+}
+const cIterable =  new CustomIterable();
+for(const item of cIterable){
+    console.log(item);
+}
