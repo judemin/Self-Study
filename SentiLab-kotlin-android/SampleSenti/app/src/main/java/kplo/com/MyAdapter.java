1d20473a8e0f0814bb9d3554b54409548ca7ed3c
민상연 <judemin2087@naver.com>
2021-07-05 17:52:10 +0900

    Lecture 9 - 뉴스 앱 만들기
    센치한 개발자 안드로이드 & 코틀린

---
+++ SentiLab-kotlin-android/SampleSenti/app/src/main/java/kplo/com/MyAdapter.java
@@ -0,0 +1,72 @@
+package kplo.com;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] localDataSet;
+
+    /**
+     *
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView _mtextView;
+        private ImageView _mImageView;
+        public ImageView mImageView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            _mtextView = (TextView) view.findViewById(R.id.row_news_tv);
+            _mImageView = (ImageView) view.findViewById(R.id.row_news_iv);
+        }
+
+        public TextView getTextView() {
+            return _mtextView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     *                by RecyclerView.
+     */
+    public MyAdapter(String[] dataSet) { // 데이터 셋의 자료형을 자유롭게 설정 가능
+        localDataSet = dataSet; // 초기 데이터 형태를 넣어주는데, 스트링 배열로 넣어줌
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.row_news, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
---
+++ SentiLab-kotlin-android/SampleSenti/app/src/main/java/kplo/com/NewsActivity.java
@@ -0,0 +1,28 @@
+package kplo.com;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsActivity extends AppCompatActivity {
+    private RecyclerView _mRecyclerView;
+    private RecyclerView.Adapter _mAdapter;
+    private RecyclerView.LayoutManager _mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        _mRecyclerView = (RecyclerView) findViewById(R.id.my_recy_view);
+
+        _mRecyclerView.setHasFixedSize(true);
+
+        _mLayoutManager = new LinearLayoutManager(this);
+        _mRecyclerView.setLayoutManager(_mLayoutManager);
+
+        _mAdapter = new MyAdapter(myDataset);
+        _mRecyclerView.setAdapter(_mAdapter);
+    }
+}
