b10a2b58049d4a95e16eea5db9353fbf83fcc09e
민상연 <judemin2087@naver.com>
2021-07-08 16:29:17 +0900

    Constraint Layout
    센치한 개발자 안드로이드 & 코틀린

---
+++ SentiLab-kotlin-android/FutureTask/app/src/main/java/com/nextop/project/futuretask/ConstActivity.kt
@@ -0,0 +1,12 @@
+package com.nextop.project.futuretask
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_constlay)
+    }
+}
