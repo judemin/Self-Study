414f816ca347e94ab7668918a4f9728fb651547d
judemin <judemin2087@naver.com>
2021-08-12 18:23:09 +0900

    Add WaveMaker project

---
+++ Euphony/WaveMaker/app/src/main/java/com/mong/wavemaker/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mong.wavemaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.mong.wavemaker.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'wavemaker' library on application startup.
+    static {
+        System.loadLibrary("wavemaker");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'wavemaker' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
